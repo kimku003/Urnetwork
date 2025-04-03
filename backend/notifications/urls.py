66377c6e9445151e55e71bfd9d650e97891774cd
kimku003 <kimku679@gmail.com>
2025-04-03 12:52:06 +0200

    rajout de toute l'app1

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import NotificationViewSet
-from .views import *
+
 router = DefaultRouter()
 router.register(r'notifications', NotificationViewSet, basename='notification')
 
